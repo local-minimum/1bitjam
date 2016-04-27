a545cb421d38412fd06d405174f300599714e8f3
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-04-27 20:19:48 +0200

    Colorsettings

---
+++ Assets/Scripts/Flashers and Emitters/BackgroundColorPicker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundColorPicker : MonoBehaviour {
+
+	[SerializeField] Color32[] colors;
+	[SerializeField] KeyCode[] keys;
+
+	Camera cam;
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			for (int i = 0; i < keys.Length; i++) {
+				if (Input.GetKeyDown (keys [i]))
+					cam.backgroundColor = colors [i];
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Flashers and Emitters/ForgroundColorPicker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ForgroundColorPicker : MonoBehaviour {
+
+	[SerializeField] Color32[] colors;
+	[SerializeField] KeyCode[] keys;
+
+	Image img;
+
+	void Start () {
+		img = GetComponent<Image> ();
+	}
+	
+
+	void Update () {
+		if (Input.anyKeyDown) {
+			for (int i = 0; i < keys.Length; i++) {
+				if (Input.GetKeyDown(keys[i]))
+					img.color = colors[i];
+			}
+		}
+	}
+}
